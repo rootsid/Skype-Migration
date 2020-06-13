f3405964c38f221b2af305262a233e87ba1e2bae
Sidharth Kaushik <sidharthkaushik@Sidharths-MacBook-Air.local>
2020-06-14 00:43:50 +0530

    Initial Commit

---
+++ tutorial/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('siginin', views.sign_in, name='signin'),
+    path('callback', views.callback, name='callback'),
+    path('signout', views.sign_out, name='signout'),
+    path('calendar', views.calendar, name='calendar'),
+]
